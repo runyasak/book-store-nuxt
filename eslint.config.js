9fe99000ca7e69afbf967b69aa09726229fdc27f
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2024-01-30 16:28:16 +0700

    add eslint config

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+// eslint.config.js
+import antfu from '@antfu/eslint-config'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const compat = new FlatCompat()
+
+export default antfu({}, ...compat.config({
+  extends: ['plugin:tailwindcss/recommended'],
+  rules: {
+    'tailwindcss/no-custom-classname': 'off',
+  },
+}))
