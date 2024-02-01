790ba4445f95d2cb146b091cbb5af2749dfa179a
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2024-02-01 12:01:20 +0700

    feat: add @nuxtjs/apollo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxt/image"],
+  modules: ['@nuxt/ui', '@nuxt/image', '@nuxtjs/apollo'],
   tailwindcss: {
     exposeConfig: true,
   },
-});
+  apollo: {
+    clients: {
+      default: {
+        httpEndpoint: 'https://library-api.alab.in/graphql/',
+      },
+    },
+  },
+})
