167e383bbd49c2e2a9d5b720d2d62a8175d147d3
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2024-02-01 12:03:15 +0700

    add BookDetailCard

---
+++ components/BookDetailCard.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts"></script>
+
+<template>
+  <UCard class="inline-block w-full">
+    <NuxtImg class="mx-auto h-[210px] w-[145px]" src="https://picsum.photos/200/300" fit="cover" />
+    <template #footer>
+      <div class="flex flex-col">
+        <span>The Housemaid</span>
+        <span>by <ULink to="#" class="text-[#2F80ED] underline">Freida McFadden</ULink></span>
+        <span>date <ULink to="#" class="text-[#2F80ED]">August 2023</ULink> </span>
+      </div>
+
+      <div class="flex gap-2 pt-2">
+        <UButton>Read</UButton>
+        <UButton color="brown">
+          Edit
+        </UButton>
+
+        <UButton color="danger">
+          Delete
+        </UButton>
+      </div>
+    </template>
+  </UCard>
+</template>
